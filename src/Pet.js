499bebd3cd7061d62f1b250af7c0465efc50b74d
Craig Hellinger <craig.hellinger@jadu.co.uk>
2020-02-11 13:58:28 +0000

    `clas` is a reserved word in javascript. Use className

--- src/Pet.js
+++ src/Pet.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Pet({ name, animal, breed }) {
   return (
-    <div class="pet">
+    <div className="pet">
       <h1>{name}</h1>
       <h2>{animal}</h2>
       <h2>{breed}</h2>
